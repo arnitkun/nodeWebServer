f4b9c05bfc296a6ee8d29666ad32d5db8aa27e5d
arnitkun <ankit.arun@getpostman.com>
2019-08-29 23:19:24 +0530

    nodeWebServer-14 added path logging ability to server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 var app = express();
 
@@ -8,8 +9,10 @@ app.set('view engine', 'hbs');//setting the view engine as hbs. But what is the 
 app.use(express.static(__dirname + '/public')); //static content serving,using middleware
 
 app.use((req, res, next) => {//next checks when the current request is done
-
-});//registers middleware
+    var now = new Date().toString();
+    console.log(`${now}: ${req.method} ${req.url}`);
+    next();
+});//app.use registers middleware
 
 hbs.registerHelper('getCurrentYear', () => {
     return new Date().getFullYear();
